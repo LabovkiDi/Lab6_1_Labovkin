9c50a4ff6f3cac79fe5169fede4c6c6f419e21c8
Дмитрий Лабовкин <labovkindi@mail.ru>
2022-01-07 17:18:05 +0300

    Добавьте файлы проекта.

---
+++ Lab6_1_Labovkin/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6_1_Labovkin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите предложение: ");
+            string s = Console.ReadLine();
+            string[] words = s.Split(' ');
+            string Maxwl = " ";
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length > Maxwl.Length)
+                    Maxwl = words[i];
+            }
+            Console.WriteLine("Самое длинное слово:{0}", Maxwl);
+            Console.ReadKey();
+        }
+    }
+}
